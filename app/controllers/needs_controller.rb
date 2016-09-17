7408b2fb8bf4d3064bf048f6835191c92acb0e28
sheroukelshiekh <sherouk2018@gmail.com>
2016-09-17 16:28:19 +0200

    my kheir

--- app/controllers/needs_controller.rb
+++ app/controllers/needs_controller.rb
@@ -1,5 +1,6 @@
 class NeedsController < ApplicationController
   before_action :set_need, only: [:show, :edit, :update, :destroy]
+   before_action :correct_user,   only: [:edit,:destroy,:showPayments]
 
   def index
       @needs = Need.where(organization_id: params[:organization_id] ,achievment_flag:false)
@@ -10,6 +11,12 @@ class NeedsController < ApplicationController
   end
   def show
   end
+
+  def showPayments
+    @need = Need.find(params[:need_id])
+  end
+
+
   def new
     @organization = Organization.find(params[:organization_id])
     @need=@organization.needs.build
@@ -20,10 +27,23 @@ class NeedsController < ApplicationController
 
   def donate
    @need = Need.find(params[:need_id])
-
    @payment= @need.payments.build(org_id: @need.organization.id,
     user_id: current_user.id , need_name:@need.title , is_recieved: false)
  end
+
+  def recieve
+     @payment = Payment.find(params[:payment_id])
+     @payment.is_recieved = true 
+     @payment.save
+     @payment.need.donated_money += @payment.donated_money
+    if @payment.need.donated_money >= @payment.need.money
+           @payment.need.achievment_flag = true
+     
+end
+    @payment.need.save
+
+
+  end
 
   def create
     @organization = Organization.find(params[:organization_id])
@@ -67,7 +87,8 @@ end
     def need_params
         params.require(:need).permit(:title, :description, :start_date, :end_date, :in_progress, :money,
          :urgent_rate, :achievment_flag ,need_images_attributes:[:caption,:photo,:id] ,
-         payments_attributes:[:comment ,:need_name, :donated_money, :user_id, :org_id , :id , :is_recieved])
+         payments_attributes:[:comment ,:need_name, :donated_money, :user_id, :org_id ,
+          :id , :is_recieved , :user_name , :user_address , :user_number])
 
       end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,37 +2,39 @@ Rails.application.routes.draw do
 
   get 'admin/home'
 
-resources :organizations do
-  collection do
-    get    'notApproved' => 'organizations#notApproved' , as: 'notApproved'
-    get    'showNotApproved/:id' => 'organizations#showNotApproved' , as: 'showNotApproved'
-    post    'approveOrg/:id' => 'organizations#approveOrg' , as: 'approveOrg'
-    delete  'disapproveOrg/:id' => 'organizations#disapproveOrg' , as: 'disapproveOrg'
-  end
-end
+
 
   resources :messages
   resources :organizations do
     resources :needs do
     get 'donation' => 'needs#donate'
+    get 'needPayments' => 'needs#showPayments'
+
+    post    'recieve/:id' => 'needs#recieve' , as: 'recieve_pay'
+  end
+    collection do
+    get    'notApproved' => 'organizations#notApproved' , as: 'notApproved'
+    get    'showNotApproved/:id' => 'organizations#showNotApproved' , as: 'showNotApproved'
+    post    'approveOrg/:id' => 'organizations#approveOrg' , as: 'approveOrg'
+    delete  'disapproveOrg/:id' => 'organizations#disapproveOrg' , as: 'disapproveOrg'
   end
     get 'Achievements' => 'needs#indexAchivements'
   end
-  resources :users
+  resources :users do
+  get '/showPayment' => 'users#showPayment'
+    get '/myKheir' => 'users#myKheir'
+  end
   resources :organization_searches
   root 'organizations#index'
-  get 'needs', to:'needs#index'
-  get 'achiev' , to:'needs#indexAchivements'
   get    '/signUp',   to: 'organizations#new'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
   get '/organization/:id/editImages', to: 'organizations#editAndaddImages', as: 'editImages'
-  get    '/donor/register',   to: 'users#new'
+  get    '/donor/register',   to: 'users#new' 
   get    '/donor/login',   to: 'sessions#new_donor'
   post   '/donor/login',   to: 'sessions#create_donor'
   delete '/donor/logout',  to: 'sessions#destroy_donor'
-  get    '/admin/login',   to: 'sessions#new_admin'
   delete '/admin/logout',  to: 'sessions#destroy_admin'
   get    '/admin/invite',  to: 'users#invite_admin'
   post    '/admin/invite',  to: 'users#invite_new_admin'
